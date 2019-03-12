9c9aa5a5d9b5cd9997cd577993eac05de5ee0880
Dhiraj <dhiraj.jamia@gmail.com>
2019-03-12 18:19:55 +0530

    Create decorator.py

---
+++ decorator.py
@@ -0,0 +1,13 @@
+def smart_divide(func):
+   def inner(a,b):
+      print("I am going to divide",a,"and",b)
+      if b == 0:
+         print("Whoops! cannot divide")
+         return
+
+      return func(a,b)
+   return inner
+
+@smart_divide
+def divide(a,b):
+    return a/b
