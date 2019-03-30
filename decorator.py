262176cd1000352ca290b8b3a9018ce1e7ba62b7
Dhiraj <dhiraj.jamia@gmail.com>
2019-03-30 15:58:49 +0530

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -2,7 +2,7 @@ def smart_divide(func):
    def inner(a,b):
       print("I am going to divide",a,"and",b)
       if b == 0:
-         print("Whoops! cannot divide")
+         print("Whoopss! cannot divide")
          return
 
       return func(a,b)
