57fa1ac9dbcd9e9583125f584d187242c4e9bc79
Dhiraj <dhiraj.jamia@gmail.com>
2019-08-27 15:00:02 +0530

    Update sqlalchemy.py

--- Python/sqlalchemy.py
+++ Python/sqlalchemy.py
@@ -6,95 +6,24 @@ students = Table(
    'students', meta, 
    Column('id', Integer, primary_key = True), 
    Column('name', String), 
-   Column('lastname', String),
+   Column('lastname', String), 
 )
 meta.create_all(engine)
+ins = students.insert()
 
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (select * from Students);
-select * from Students LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT type,name,sql,tbl_name,'0' AS temp FROM sqlite_master UNION SELECT type,name,sql,tbl_name,'1' AS temp FROM sqlite_temp_master;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (select * from Students);
-select * from Students LIMIT 0, 50000;
-SELECT type,name,sql,tbl_name,'0' AS temp FROM sqlite_master UNION SELECT type,name,sql,tbl_name,'1' AS temp FROM sqlite_temp_master;
-SELECT COUNT(*) FROM (select * from Students);
-select * from Students LIMIT 0, 50000;
-SELECT COUNT(*) FROM (select * from Students);
-select * from Students LIMIT 0, 50000;
+conn = engine.connect()
+conn.execute(students.insert(), [
+   {'name':'Rajiv', 'lastname' : 'Khanna'},
+   {'name':'Komal','lastname' : 'Bhandari'},
+   {'name':'Abdul','lastname' : 'Sattar'},
+   {'name':'Priya','lastname' : 'Rajhans'},
+])
 
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT type,name,sql,tbl_name,'0' AS temp FROM sqlite_master UNION SELECT type,name,sql,tbl_name,'1' AS temp FROM sqlite_temp_master;
+s = students.select()
+conn = engine.connect()
+result = conn.execute(s)
+
+for row in result:
+   print (row)
 
 
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC);
-SELECT `_rowid_`,* FROM `students` WHERE `id` LIKE '%1%' ESCAPE '\' AND `name` LIKE '%dhiraj%' ESCAPE '\' AND `lastname` LIKE '%kumar%' ESCAPE '\' ORDER BY `_rowid_` ASC LIMIT 0, 50000;
-SELECT COUNT(*) FROM (select * from 'class');
-select * from 'class' LIMIT 0, 50000;
-SELECT COUNT(*) FROM (SELECT COUNT(*) FROM (select * from 'class'));
-SELECT COUNT(*) FROM (select * from 'class') LIMIT 0, 50000;
