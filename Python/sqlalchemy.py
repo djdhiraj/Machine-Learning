41d88c2285311cc72a6c3f2c84e45e8b369efb5b
Dhiraj <dhiraj.jamia@gmail.com>
2019-08-27 16:37:46 +0530

    Update sqlalchemy.py

--- Python/sqlalchemy.py
+++ Python/sqlalchemy.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey
 engine = create_engine('sqlite:///college.db', echo = True)
 meta = MetaData()
 
@@ -15,6 +16,14 @@ classes = Table(
    Column('Class_name', String), 
    Column('student_id', Integer),
 )
+addresses = Table(
+   'addresses', meta, 
+   Column('id', Integer, primary_key = True), 
+   Column('st_id', Integer, ForeignKey('students.id')), 
+   Column('postal_add', String), 
+   Column('email_add', String))
+
+
 meta.create_all(engine)
 ins = students.insert()
 
@@ -37,7 +46,13 @@ conn.execute(classes.insert(), [
    {'Class_name':'5th','student_id' : 4},
 ])
 
-
+conn.execute(addresses.insert(), [
+   {'st_id':1, 'postal_add':'Shivajinagar Pune', 'email_add':'ravi@gmail.com'},
+   {'st_id':1, 'postal_add':'ChurchGate Mumbai', 'email_add':'kapoor@gmail.com'},
+   {'st_id':3, 'postal_add':'Jubilee Hills Hyderabad', 'email_add':'komal@gmail.com'},
+   {'st_id':5, 'postal_add':'MG Road Bangaluru', 'email_add':'as@yahoo.com'},
+   {'st_id':2, 'postal_add':'Cannought Place new Delhi', 'email_add':'admin@khanna.com'},
+])
 s = students.select()
 conn = engine.connect()
 result = conn.execute(s)
