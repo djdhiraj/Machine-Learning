a2e365da46755697413f742c3e48a332741d5ae6
Dhiraj <dhiraj.jamia@gmail.com>
2019-08-27 16:22:53 +0530

    Create Update_sqlalchemy.py

---
+++ Python/Update_sqlalchemy.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+engine = create_engine('sqlite:///college.db', echo = True)
+meta = MetaData()
+
+students = Table(
+   'students', 
+   meta, 
+   Column('id', Integer, primary_key = True), 
+   Column('name', String), 
+   Column('lastname', String), 
+)
+
+conn = engine.connect()
+stmt=students.update().where(students.c.lastname=='Khanna').values(lastname='Kapoor')
+conn.execute(stmt)
+s = students.select()
+conn.execute(s).fetchall()
